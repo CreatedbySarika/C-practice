c64b9dd21929035d7fc1f4abbb1c6b93a5527142
CreatedBySarika <sahsarika2005@gmail.com>
2026-07-14 22:36:25 +0100

    incorrect

---
+++ Ch1_getting_started/temperature_convert.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    float farahnite;
+    float celcius = -17.22;
+
+    printf("Enter temperature in farahnite :");
+    scanf("%f",&farahnite);
+    /* the output is incorrect as it is not the correct incrementation*/
+    printf("Temperature in celcius: %.0f",farahnite*celcius);
+
+
+
+
+
+
+}
