6c5c4f83a07df367b6ff78625e6b142235ecdf4d
CreatedBySarika <sahsarika2005@gmail.com>
2026-07-19 01:24:33 +0100

    inverted-tri

--- CH4_Loops/alphabet_tri.c
+++ CH4_Loops/alphabet_tri.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 
 int main(){
-    int num = 72;
+    int num = 72,start = 65;
 
     for(int i = 65 ;i<=num;num--)
-    {
-               
+    {               
         for(int j =i;j<num;j++)
         {
             printf("%c " ,j);
         }
+    printf("\n");
+    
+    
+    }
+/*this prints the the triangle inverted*/
+    for(int k = 71;k>=65;k--)
+    {
+        //this is for indentation 
+        for (int ind = 71; ind > k;ind--)
+        {
+            printf("  ");
+        }
+        for (int l =k;l>=start;l--)
+        {
+            
+            printf("%c ",l);
+        }
+        
     printf("\n");
     }
 }
