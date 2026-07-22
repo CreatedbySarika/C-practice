6996a80ef61615c3522bda32956f639b2069085a
CreatedBySarika <sahsarika2005@gmail.com>
2026-07-22 12:16:59 +0100

    factorial added

--- CH5_Switch_case/calculator.c
+++ CH5_Switch_case/calculator.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+int factorial(int n){
+    int x;
+    if(n==1)
+        return 1;
+    else
+        x= n * factorial(n-1);
+    
+    return x;
+
+}
 int main(){
     int choice;
     while(choice!=4)
     {
-        printf("Enter what would you like to calculate : press\n[1]Factorial of a number\n[2]Prime or not\n[3]odd or even\n[4]Exit\n");
+        printf("what would you like to calculate press:\n[1]Factorial of a number\n[2]Prime or not\n[3]odd or even\n[4]Exit\n");
         scanf("%d",&choice);
 
         switch(choice)
@@ -11,6 +21,10 @@ int main(){
             case 1:
             {
                 printf("Factorial Selected\n");
+                int num;
+                printf("Enter a number: ");
+                scanf("%d",&num);
+                printf("The factorial of the %d is %d\n********************************\n",num,factorial(num));
                 break;
             }
                 
