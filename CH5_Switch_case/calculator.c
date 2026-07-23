56f5fb0ac12882e0ec96816f269b18cdc9294148
CreatedBySarika <sahsarika2005@gmail.com>
2026-07-23 11:11:59 +0100

    prime added

--- CH5_Switch_case/calculator.c
+++ CH5_Switch_case/calculator.c
@@ -9,8 +9,42 @@ int factorial(int n){
     return x;
 
 }
+void oddEven(int n){
+    if(n%2==0)
+        printf("%d is an Even Number",n);
+    else
+        printf("%d is a odd Number ",n);
+
+}
+void ifprime(int n){
+    if(n==1)
+        printf("1 is neither prime nor composite");
+    
+    if(n==2)
+            printf("2 is a prime number");
+    for(int i=2;i<n;i++)
+    {     
+
+        if(n%i==0)
+        {
+            printf("%d is a composite number",n);
+            break;
+        }
+        
+        else
+        {
+            printf("%d is a prime number",n);
+            break;
+        }    
+    }
+              
+
+    
+
+}
 int main(){
     int choice;
+    int num;
     while(choice!=4)
     {
         printf("what would you like to calculate press:\n[1]Factorial of a number\n[2]Prime or not\n[3]odd or even\n[4]Exit\n");
@@ -21,7 +55,7 @@ int main(){
             case 1:
             {
                 printf("Factorial Selected\n");
-                int num;
+                
                 printf("Enter a number: ");
                 scanf("%d",&num);
                 printf("The factorial of the %d is %d\n********************************\n",num,factorial(num));
@@ -31,12 +65,22 @@ int main(){
             case 2:
             {
                 printf("prime check selected\n");
+                printf("Enter a number: ");
+                scanf("%d",&num);
+                ifprime(num);
+                printf("\n******************************\n\n");
+
+
                 break;
             }
                 
             case 3:
             {
                 printf("odd or even\n");
+                printf("Enter a number:");
+                scanf("%d",&num);
+                oddEven(num);
+                printf("\n******************************\n\n");
                 break;
             }
                 
